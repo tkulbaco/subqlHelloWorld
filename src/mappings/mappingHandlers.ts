a772b7244a4e5b31f6ab524c7f86f384befda839
Tatsiana Kulbaka <tkulbaco@gmail.com>
2022-01-21 06:24:43 +0300

    first commit
    Signed-off-by: Tatsiana Kulbaka <tkulbaco@gmail.com>

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,16 @@
+import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+import {StarterEntity} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
+
+
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+    //Create a new starterEntity with ID using block hash
+    let record = new StarterEntity(block.block.header.hash.toString());
+    //Record block number
+    record.field1 = block.block.header.number.toNumber();
+    await record.save();
+}
+
+
+
+
